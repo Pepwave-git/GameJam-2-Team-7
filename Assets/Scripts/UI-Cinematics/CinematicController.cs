fafdbddf5828bd1d1710eb16f023a942e0578e81
Pepwave-git <lh.pepwave@gmail.com>
2024-11-05 10:14:53 -0500

    Cinematica arreglada

--- Assets/Scripts/UI-Cinematics/CinematicController.cs
+++ Assets/Scripts/UI-Cinematics/CinematicController.cs
@@ -2,13 +2,18 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Playables; // Asegúrate de incluir esto para usar Timeline
+using UnityEngine.Video;
 
 public class CinematicController : MonoBehaviour
 {
+    public VideoClip cinematic;
     public PlayableDirector director; // Referencia al PlayableDirector
+    public VideoPlayer reproductor;
 
     void Start()
     {
+        cinematic = Resources.Load<VideoClip>("mp4/Comp 1");
+        reproductor.clip = cinematic;
         // Reproducir la cinemática al iniciar
         director.Play();
         // Llama a la función para cargar la escena del juego después de que la cinemática termine
